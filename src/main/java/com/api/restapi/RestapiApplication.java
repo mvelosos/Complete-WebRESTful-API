519d9538ff4269371c10ced66782cd2809131025
Mateus Veloso <mfvelosos@gmail.com>
2018-06-01 18:01:32 -0300

    Creating Product domain and associations many to many

--- src/main/java/com/api/restapi/RestapiApplication.java
+++ src/main/java/com/api/restapi/RestapiApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.api.restapi.domain.Category;
+import com.api.restapi.domain.Product;
 import com.api.restapi.repositories.CategoryRepository;
+import com.api.restapi.repositories.ProductRepository;
 
 @SpringBootApplication
 public class RestapiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoriaRepository;
+	
+	@Autowired
+	private ProductRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
@@ -26,7 +31,19 @@ public class RestapiApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Info");
 		Category cat2 = new Category(null, "Office");
 		
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 	}
 	
 }
