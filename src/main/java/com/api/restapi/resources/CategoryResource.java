0820b40a23faed0ce8ac348645ceb570006edd0d
Mateus Veloso <mfvelosos@gmail.com>
2018-06-01 16:21:04 -0300

    Testing first domain class

--- src/main/java/com/api/restapi/resources/CategoryResource.java
+++ src/main/java/com/api/restapi/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.api.restapi.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.restapi.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST ok!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Info");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
