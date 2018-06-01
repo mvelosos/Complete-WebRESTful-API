e2ff17c12f2a8b891e00fa24b7220431fa2151e2
Mateus Veloso <mfvelosos@gmail.com>
2018-06-01 17:21:02 -0300

    Populating database when starting

--- src/main/java/com/api/restapi/RestapiApplication.java
+++ src/main/java/com/api/restapi/RestapiApplication.java
@@ -1,12 +1,32 @@
 package com.api.restapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.api.restapi.domain.Category;
+import com.api.restapi.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Info");
+		Category cat2 = new Category(null, "Office");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	}
+	
 }
